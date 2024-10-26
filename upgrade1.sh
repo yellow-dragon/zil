3f16e1f52202b68b2f983db594c1cacdb54ca071
yellow-dragon <44664703+yellow-dragon@users.noreply.github.com>
2024-10-26 08:42:24 +0700

    Update upgrade1.sh

--- upgrade1.sh
+++ upgrade1.sh
@@ -2,16 +2,16 @@
 cd /app-data/miners 
 
 rm -f startup-script.sh
-rm -f a0-oc-script.sh
+rm -f a0-oc-scriptQUBIC.sh
 rm -f a1-oc-script.sh
 rm -f zil-oc-script.sh 
 
 wget https://raw.githubusercontent.com/yellow-dragon/zil/main/startup-script.sh
-wget https://raw.githubusercontent.com/yellow-dragon/zil/main/a0-oc-script.sh
+wget https://raw.githubusercontent.com/yellow-dragon/zil/main/a0-oc-scriptQUBIC.sh
 wget https://raw.githubusercontent.com/yellow-dragon/zil/main/a1-oc-script.sh
 wget https://raw.githubusercontent.com/yellow-dragon/zil/main/zil-oc-script.sh
 
 chmod +x startup-script.sh
-chmod +x a0-oc-script.sh
+chmod +x a0-oc-scriptQUBIC.sh
 chmod +x a1-oc-script.sh
 chmod +x zil-oc-script.sh
