e430b420a7d5d613b2fdb05b6bdd47068c14e308
yellow-dragon <44664703+yellow-dragon@users.noreply.github.com>
2024-10-26 19:58:04 +0700

    Update and rename a0-oc-scriptQUBIC.sh to a0.sh

--- a0-oc-scriptQUBIC.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-screen -S H9 -dm bash -c "/app-data/miners/apoolminer-2.6.1/apoolminer --algo qubic --account CP_e2sig0aa15 --worker $(hostname) --pool qubic1.hk.apool.io:3334 --rest --cpu-off"
