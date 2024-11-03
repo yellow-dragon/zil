b5105defefd019f9b2a3373b7bc1cc6b2e909eaa
yellow-dragon <44664703+yellow-dragon@users.noreply.github.com>
2024-11-03 16:34:35 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,5 +3,5 @@ echo "ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"
 killall apoolminer
 killall aleominer
 killall a0.sh
-sleep 5
+sleep 1
 screen -S a0 -dm bash -c "/app-data/miners/a0.sh"
