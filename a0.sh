29239fb3e6bea2aff8a62513d481ecc0c814f703
yellow-dragon <44664703+yellow-dragon@users.noreply.github.com>
2024-11-06 02:18:38 +0700

    Update a0.sh

--- a0.sh
+++ a0.sh
@@ -4,18 +4,18 @@ qubic=0
 aleo=0
 
 while true; do
-  response=$(curl -s http://qubic1.hk.apool.io:8001/api/qubic/epoch_challenge)
+  response=$(curl -s http://8.217.123.224:8001/api/qubic/epoch_challenge)
   if echo "$response" | grep -q "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; then
     if [ "$aleo" -eq 0 ]; then
       killall apoolminer      
-      screen -S aleo -dm bash -c "/app-data/miners/aleominer-3.0.12/aleominer -u stratum+ssl://aleo-asia.f2pool.com:4420 -w hoanglong.$(hostname)"      
+      screen -S aleo -dm bash -c "/app-data/miners/aleominer-3.0.12/aleominer -u stratum+ssl://172.65.186.4:4420 -w hoanglong.$(hostname)"      
       aleo=1
       qubic=0
     fi
   else
     if [ "$qubic" -eq 0 ]; then
       killall aleominer 
-      screen -S qubic -dm bash -c "/app-data/miners/apoolminer-2.6.2/apoolminer --algo qubic --account CP_e2sig0aa15 --worker $(echo $(hostname) | awk '{print substr($0, 1, 15)}') --pool 8.217.123.224:3334 --rest --cpu-off"      
+      screen -S qubic -dm bash -c "/app-data/miners/apoolminer-2.6.2/apoolminer --algo qubic --account CP_e2sig0aa15 --worker $(echo $(hostname) | awk '{print substr($0, 1, 15)}') --pool 8.217.123.224:3334 --rest --port 5500 --cpu-off"      
       qubic=1
       aleo=0
     fi
